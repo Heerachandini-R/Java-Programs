a5a34c916ac30bb95e49d5519fe6cdb9313383e0
rheer <rheer@DESKTOP-845EPTU.Dlink>
2020-04-24 03:50:03 +0530

    Code Commit

---
+++ Java practices/src/com/java/heera/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.java.heera;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+        System.out.print("by ");
+        System.out.println("Heera");
+        MethodTest.writeName();
+	}
+
+}
