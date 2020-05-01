986d463bffdef2e9ec65dcbb0643f210dd7b643a
rheer <rheer@DESKTOP-845EPTU.Dlink>
2020-05-01 19:57:49 +0530

    Code Commit

---
+++ StringManipulation/src/com/java/heera/StringConcatenation.java
@@ -0,0 +1,15 @@
+package com.java.heera;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		String str1="Heera";
+		String str2="Chandini";
+		//concatenation using arithmetic "+" operator
+		String str3=str1+str2;
+		System.out.println(str3);
+		//concatenation using "string.concat()" method
+		System.out.println(str2.concat(str1));
+		System.out.println(str1.concat(str2));
+	}
+}
